a7671f874eac09b02b1a3fd81d7b3ced28dec2d4
Joshua Sharp <joshuafsharp@gmail.com>
2022-12-24 23:28:26 +1100

    feat: styling updates and config changes

--- src/app/dashboard/library/playlists.server.tsx
+++ src/app/dashboard/library/playlists.server.tsx
@@ -1,6 +1,7 @@
 "server-only";
 
 import Image from "next/image";
+import Link from "next/link";
 import { spotifyApi } from "~/common/spotify/server";
 
 interface Props {
@@ -22,27 +23,50 @@ export const LibraryPlaylists = async ({ token }: Props) => {
 
   return (
     <section className="mt-12">
-      <h2 className="mb-8 text-xl font-semibold leading-tight tracking-wide dark:text-white">
-        Playlists
-      </h2>
+      <div className="mb-8 flex items-center justify-between">
+        <h2 className="text-xl font-semibold leading-tight tracking-wide dark:text-white">
+          <Link
+            href="/dashboard/library/playlists"
+            aria-label="View all playlists"
+            className="hover:underline"
+          >
+            Playlists
+          </Link>
+        </h2>
 
-      <ul className="flex items-center space-x-6 overflow-x-auto scroll-smooth">
+        {/* TODO: Playlists index page */}
+        <Link
+          href="/dashboard/library/playlists"
+          aria-label="View all playlists"
+          className=" uppercase tracking-wide hover:text-zinc-300 hover:underline dark:text-zinc-200"
+        >
+          View all
+        </Link>
+      </div>
+
+      <ul className="flex items-stretch space-x-2 overflow-x-auto scroll-smooth">
         {playlists.map((playlist) => (
           <li
             key={playlist.id}
-            className="w-36 flex-shrink-0 self-start dark:text-white"
+            className="flex w-36 flex-shrink-0 self-stretch rounded-lg transition-colors hover:bg-zinc-900 dark:text-white"
           >
-            {playlist.images[0] && (
-              <Image
-                alt={playlist.name}
-                src={playlist.images[0].url}
-                width={playlist.images[0]?.width}
-                height={playlist.images[0]?.height}
-                className="mb-2 rounded-md"
-              />
-            )}
+            <Link href={`/dashboard/library/playlists/${playlist.id}`}>
+              <div className="flex flex-col p-4">
+                {playlist.images[0] && (
+                  <Image
+                    alt={playlist.name}
+                    src={playlist.images[0].url}
+                    width={playlist.images[0]?.width}
+                    height={playlist.images[0]?.height}
+                    className="mb-2 rounded-md"
+                  />
+                )}
 
-            <div className="font-semibold">{playlist.name}</div>
+                <div className="flex-grow font-semibold line-clamp-3">
+                  {playlist.name}
+                </div>
+              </div>
+            </Link>
           </li>
         ))}
       </ul>
